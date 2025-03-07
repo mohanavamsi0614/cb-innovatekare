610e4329594fc9fbfb4eecf76122360223543d78
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-03-07 17:13:04 +0530

    Done

--- src/TeamPanel.jsx
+++ src/TeamPanel.jsx
@@ -1179,7 +1179,7 @@ const restartTour = () => {
                     )
                 )}
                 <footer className="mt-auto border-t border-gray-800">
-                    <p className="text-center p-4 text-white text" >Made and designed By Vamsi And Sushma</p>
+                    <p className="text-center p-4 text-white text">Made with 💖 By Coding Blocks KARE</p>
                 </footer>
             </div>
         </div>
